920be180890e01de7747ad75e550acf5c3d011e6
LinearDash <asandesh495@gmail.com>
2025-06-03 16:05:58 +0545

    feat: implement login functionality in DashLogin component; add state management for username and password

--- Frontend/src/pages/Dashboard/DashLogin.jsx
+++ Frontend/src/pages/Dashboard/DashLogin.jsx
@@ -1,6 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 
 const DashLogin = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const ButtonAction = () => {
+    console.log(`Username: ${username}, Password: ${password}`);
+  };
+
   return (
     <div className="min-h-screen flex items-center justify-center">
       <div className="bg-neutral-400 h-100 w-200 rounded-3xl p-10 flex flex-col items-center">
@@ -9,13 +16,22 @@ const DashLogin = () => {
           type="text"
           placeholder="Username"
           className="mb-9 p-2 rounded border border-gray-300 bg-amber-50"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
         />
         <input
           type="password"
           placeholder="Password"
           className="mb-9 p-2 rounded border border-gray-300 bg-amber-50"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
         />
-        <button className="bg-yellow-200 w-20 rounded-lg mt-5">login</button>
+        <button
+          className="bg-yellow-200 w-20 rounded-lg mt-5"
+          onClick={ButtonAction}
+        >
+          login
+        </button>
       </div>
     </div>
   );
