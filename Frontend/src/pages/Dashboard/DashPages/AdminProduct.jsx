af6c24bc2746d95f54d4287997f5d2a7c8715323
LinearDash <asandesh495@gmail.com>
2025-06-03 16:12:16 +0545

    feat: enhance AdminProduct component layout; add placeholders for categories and product management

--- Frontend/src/pages/Dashboard/DashPages/AdminProduct.jsx
+++ Frontend/src/pages/Dashboard/DashPages/AdminProduct.jsx
@@ -1,7 +1,22 @@
 import React from "react";
 
 const AdminProduct = () => {
-  return <div className="bg-amber-200 min-h-screen">AdminProduct</div>;
+  return (
+    <div className="bg-amber-200 min-h-screen">
+      {/* will use map to make show all product in all catogory */}
+      <div>
+        catogory
+        <div>product</div>
+        {/* need a product card */}
+      </div>
+      <div>
+        <p>Add new catogory</p>
+        {/* need a drop down ig */}
+        <p> Add new product</p>
+        {/* need a add product page */}
+      </div>
+    </div>
+  );
 };
 
 export default AdminProduct;
