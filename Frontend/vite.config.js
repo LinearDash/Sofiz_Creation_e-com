6bc7930ed4cbfdeb4a2bda32389c455be6b3069e
LinearDash <asandesh495@gmail.com>
2025-07-10 14:42:22 +0545

    fix: update API proxy target in vite.config.js to use environment variable for backend URL

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -4,12 +4,12 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),   tailwindcss()],
-  server:{
-    port:3000,
-    proxy:{
-      "/api":{
-        target:"http://localhost:5000",
+  plugins: [react(), tailwindcss()],
+  server: {
+    port: 3000,
+    proxy: {
+      "/api": {
+        target: process.env.BACKEND_BASE_URL,
         // changeOrigin: true
       }
     }
